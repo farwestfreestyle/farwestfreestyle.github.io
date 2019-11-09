d88085b7c2849f3e7ac6f5178b502bc190335552
Angelo Calub <angelo.calub@gmail.com>
2019-11-09 14:56:23 -0800

    new version

---
+++ _nuxt/35b6852870e37fc7fe74.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{501:function(e,t,n){"use strict";n.r(t);n(38);var r={created:function(){this.$router.replace("/")}},c=n(41),component=Object(c.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
