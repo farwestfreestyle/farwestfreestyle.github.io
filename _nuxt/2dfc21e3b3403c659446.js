f37ef04453468672bff49d1fc7ef5c3ded301c6f
Angelo Calub <angelo.calub@gmail.com>
2019-11-10 22:32:17 -0800

    release

--- _nuxt/2dfc21e3b3403c659446.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{502:function(e,t,n){"use strict";n.r(t);n(38);var r={created:function(){this.$router.replace("/")}},c=n(41),component=Object(c.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
