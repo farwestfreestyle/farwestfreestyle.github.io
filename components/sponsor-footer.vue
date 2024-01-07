12625fb7ea5d60c384b046cc0ae33c95f0f8a4ff
Angelo Calub <angelo.calub@gmail.com>
2024-01-07 09:37:17 -0800

    update mtn lotus yoga link

--- components/sponsor-footer.vue
+++ components/sponsor-footer.vue
@@ -2,7 +2,7 @@
   <footer>
     <v-container>
       <v-row justify="center" align="center" class="sponsors">
-        <a href="https://https://www.mountainlotusyoga.com"><v-img contain src="/images/mountain-lotus-yoga.png" max-height="150" max-width="150" border="0"></v-img></a>
+        <a href="https://mountainlotusyoga.com"><v-img contain src="/images/mountain-lotus-yoga.png" max-height="150" max-width="150" border="0"></v-img></a>
         <v-img contain src="/images/ovfree-logo.png" max-height="150" max-width="150" border="0"></v-img>
         <a href="https://www.idoneusa.com/default.asp"><v-img contain max-height="60" max-width="230" src="/images/idone-logo.svg" border="0"></v-img></a>
         <a href="https://www.tahoesportshub.com"><v-img contain max-height="150" max-width="150" src="/images/tahoe-sportshub-logo.svg" border="0"></v-img></a>
